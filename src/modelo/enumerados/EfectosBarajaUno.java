bbdf47aa3950a2f487391be4e2cdd11508760740
camilascarnatto <scarnattocamila@gmail.com>
2022-09-20 15:00:04 -0300

    ahora cada efecto tiene su respectivo puntaje para el juego

--- src/modelo/enumerados/EfectosBarajaUno.java
+++ src/modelo/enumerados/EfectosBarajaUno.java
@@ -2,9 +2,21 @@ package modelo.enumerados;
 
 
 public enum EfectosBarajaUno {
-    MASDOS,
-    MASCUATRO,
-    SALTO,
-    REVERSO,
-    CAMBIOCOLOR;
+    MASDOS(20),
+    MASCUATRO(50),
+    SALTO(20),
+    REVERSO(20),
+    CAMBIOCOLOR(50);
+	
+	
+	private int puntos;
+
+	private EfectosBarajaUno(int puntos) {
+		this.puntos = puntos;
+	}
+	
+	public int getPuntos() {
+		return puntos;
+	}
+	
 }
